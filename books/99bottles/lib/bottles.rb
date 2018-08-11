40707b0ae7e58334ac1bf89cd11ad89bde6a2982
Peter Toth <tothpeter08@gmail.com>
2018-08-11 10:24:34 +0200

    Refactor my own solution

--- books/99bottles/lib/bottles.rb
+++ books/99bottles/lib/bottles.rb
@@ -26,36 +26,29 @@ class Bottles
   private
 
   def generate_first_sentance(number_of_bottles)
-    prefix = bottles_prefix(number_of_bottles)
-    bottles = bottles(number_of_bottles)
+    x_bottles_of_beer = x_bottles_of_beer(number_of_bottles)
 
-    "#{prefix} #{bottles} of beer on the wall, #{prefix} #{bottles} of beer.".capitalize
+    "#{x_bottles_of_beer} on the wall, #{x_bottles_of_beer}.".capitalize
   end
 
   def generate_second_sentance(number_of_bottles)
     if number_of_bottles > -1
-      prefix = bottles_prefix(number_of_bottles)
-      bottles = bottles(number_of_bottles)
+      x_bottles_of_beer = x_bottles_of_beer(number_of_bottles)
+      one_or_it         = number_of_bottles > 0 ? 'one' : 'it'
 
-      one_or_it = number_of_bottles > 0 ? 'one' : 'it'
-
-      "Take #{one_or_it} down and pass it around, #{prefix} #{bottles} of beer on the wall."
+      "Take #{one_or_it} down and pass it around, #{x_bottles_of_beer} on the wall."
     else
       'Go to the store and buy some more, 99 bottles of beer on the wall.'
     end
   end
 
-  def bottles(count)
-    sign_of_plural = count == 1 ? '' : 's'
-
-    "bottle#{sign_of_plural}"
-  end
-
-  def bottles_prefix(count)
-    if count.zero?
-      'no more'
+  def x_bottles_of_beer number_of_bottles
+    if number_of_bottles == 0
+      'no more bottles of beer'
+    elsif number_of_bottles == 1
+      '1 bottle of beer'
     else
-      count
+      "#{number_of_bottles} bottles of beer"
     end
   end
 end
